a089e40c6f5cfdb580a6d7b2344c4c1a8090de89
JDO <atharos@vivaldi.net>
2019-11-16 10:22:44 +0000

    Adding further documentation and moving test elements to a vignette to avoid build failure

--- R/Yaml_integrations.R
+++ R/Yaml_integrations.R
@@ -1,42 +1,32 @@
-library(yaml)
-
-
-here::here("dict.yml") %>%
-        read_yaml() -> tester
-
-here::here("dict_spec.yml") %>%
-        read_yaml() -> spec
 
 
 
-write_yaml(tester, here::here("dict.yml") )
 
 
-## Testing for integer
-
-tester <- list()
-appendIntegerMeta <- function(a_dataframe, a_list, a_column){
-        if(is.null(a_list[[a_column]])) {
-                a_list[[a_column]] <- list()
-                }
-
-        a_list[[a_column]][["min"]] <- min(a_dataframe[[a_column]])
-        a_list[[a_column]][["max"]] <- max(a_dataframe[[a_column]])
-        a_list[[a_column]][["name"]] <- a_column
-        a_list
-}
-test_data <- iris %>% as.data.table()
-
-
-test_list <- test_data %>%
-        appendIntegerMeta(tester, "Sepal.Length")
-
+#' Function to determine the range of an integer list
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
 utilRange <- function(x) {
         max(x, na.rm = T) - min(x, na.rm = T)
-        }
+}
+
+
+#' Function to evaluate a string name of function on some specified data
+#'
+#' @param x
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
 utilEvalOnData <- function(x, data) {
         eval(as.name(x))(data)
 }
 
 
-tester$AccountLevel$Sepal.Length = spec$AccountLevel$integer %>% purrr::map(utilEvalOnData, data = iris$Sepal.Length)
