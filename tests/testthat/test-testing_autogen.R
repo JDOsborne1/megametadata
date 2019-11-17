468c48d101fd069f2e41730e3e089b565c9eafac
JDO <atharos@vivaldi.net>
2019-11-17 23:02:04 +0000

    Expanding tests to include the Automatic expansion of the dict by the specification

--- tests/testthat/test-testing_autogen.R
+++ tests/testthat/test-testing_autogen.R
@@ -8,3 +8,13 @@ test_that("Autogeneration works", {
   expect_equal(test_dict$Petal.Length$data_category, "PII/Value")
 })
 
+test_that("Auto Expansion by specification works", {
+        new_dict <- metaUpdateDictWithSpec(tester, spec, iris, "AccountLevel")
+  expect_equal(new_dict$AccountLevel$Species$levels, c("setosa", "versicolor", "virginica"))
+  expect_equal(new_dict$AccountLevel$Species$ordered , FALSE)
+  expect_equal(new_dict$AccountLevel$Sepal.Width$minimum , 2)
+  expect_equal(new_dict$AccountLevel$Sepal.Width$uniqueness , uniqueness(iris$Sepal.Width), tolerance = 0.0002)
+  expect_equal(new_dict$AccountLevel$Petal.Length$range , utilRange(iris$Petal.Length))
+
+})
+
