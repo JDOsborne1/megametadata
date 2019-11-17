74623c4a742bf517df49db345d3342e2814a9de3
JDO <atharos@vivaldi.net>
2019-11-17 23:28:30 +0000

    Documenting datasets and Package

---
+++ R/data_docs.R
@@ -0,0 +1,29 @@
+## The package documentation
+
+#' Data Dictionary generation and interaction
+#'
+#' This package contains functions and examples for using and expanding data
+#' dictionaries
+#' @docType package
+#' @name megametadata
+#' @aliases meta-monster
+NULL
+
+## The dataset documentation
+
+
+#' Example Specification
+#'
+#' An example specification which can be used to expand or update the initial
+#' phase of the data dictionary
+#'
+#' @format A list of lists with each Level of the data and each class of column
+"spec"
+
+#' Example Dict config
+#'
+#' An example Dictionary which can be used as an example to expand from or as a
+#' testing object
+#'
+#' @format A list of lists with each Level of the data and each class of column
+"tester"
