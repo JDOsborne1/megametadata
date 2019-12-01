f0d5cfd67323b253cab7fab09e9751a4406940b2
JDO <atharos@vivaldi.net>
2019-12-01 12:39:32 +0000

    Added error handling tests

--- tests/testthat/test-ultraspoofr-integration.R
+++ tests/testthat/test-ultraspoofr-integration.R
@@ -8,3 +8,8 @@ test_that("Nominal Split works: USR example", {
         expect_equal(split_data$nominal, "Species")
         expect_equal(split_data$continuous, c("Sepal.Length", "Sepal.Width",  "Petal.Length", "Petal.Width" ))
 })
+test_that("Nominal Split error handling works", {
+        expect_error(metaSplitNominal(list()), "Extract Level Required for list dicts")
+
+        expect_error(metaSplitNominal(factor(c(10,2,1))), "No method to support that structure of metadata")
+})
